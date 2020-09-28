40790c642d8e5085d67d9c7d2f0e27a7b4e85868
Hannah Turner <hturner0696@gmail.com>
2020-09-28 11:59:43 -0400

    Appointment and Article Pages
    Added appointment and article pages; added interactivity

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ setTimeout(() => {
     document.getElementById('splash').classList.toggle('fade');
 }, 2000);
 
+setTimeout(() => {
+    document.getElementById('splash').classList.toggle('gone');
+}, 2800);
+
